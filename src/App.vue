a0b67ecfd997bf6eff72aba8a572a8f538cc7735
prabin1122 <prabinkarki741@gmail.com>
2022-09-08 15:07:54 +0545

    first

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <Header/>
+    <div class="container">
+      <router-view/>
+    </div>
+  </div>
+</template>
+<script>
+import Header from "@/components/Header.vue";
+
+export default {
+  components: {
+    Header
+  }
+};
+</script>
+
+<style>
+body {
+  background: #ffffff;
+}
+.container {
+  max-width: 900px;
+}
+.card {
+  background: #000000;
+  box-shadow: 12px 2px 10px #000;
+}
+.card-body {
+  color: #f9ffee;
+}
+.card-body a {
+  color: #ffffff;
+}
+.card-body a:hover {
+  color: rgb(213, 213, 213);
+  text-shadow: 2px 2px 22px white;
+  text-decoration: none;
+}
+</style>
---
+++ src/views/PostList.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="post-list">
+    <div
+      class="card mb-3"
+      v-for="post of posts"
+      :key="post.id"
+    >
+      <div class="card-body">
+        <h3>
+          <router-link :to="`/post-details/${post.id}`">{{post.title}}</router-link>
+        </h3>
+        <p>{{post.body}}</p>
+        <button @click="$emit('del-post', post.id)" class="delete_button">Delete</button>
+      </div>
+    </div>
+    <div class="card mb-3" v-if="errors && errors.length">
+      <div class="card-body">
+        <p v-for="(error, index) of errors" :key="index">{{error.message}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PostList",
+  props: ["posts"]
+};
+</script>
+
+<style>
+  .delete_button{
+    background-color: black;
+    border: solid 2px white;
+    color: white;
+    transition: all 0.6s;
+  }
+  .delete_button:hover{
+    background-color: white;
+    border: solid 2px white;
+    color: black;
+  }
+</style>
